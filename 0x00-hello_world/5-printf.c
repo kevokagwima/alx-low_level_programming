0ebd0ae299b1cf4766ffe46e8062aec7b451b369
kevokagwima <kevokagwima@gmail.com>
2024-06-20 23:43:48 -0700

    Test

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * main - this function uses printf to print out a text and then returns a value
+ */
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
