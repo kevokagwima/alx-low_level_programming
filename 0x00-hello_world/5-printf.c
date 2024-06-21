13cecd736c601d8a1e48cab0395cd81d06fd6801
kevokagwima <kevokagwima@gmail.com>
2024-06-20 23:59:15 -0700

    Test

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,14 +2,12 @@
 
 /**
  * main - this function uses printf to print out a text
+ * return (0) - return te value 0
  */
 
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-/*
- * return -Returns 0
-*/
 return (0);
 }
 
