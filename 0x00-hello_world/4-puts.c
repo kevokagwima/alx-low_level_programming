08721ef47ad329a2b9c22c7705684387e427b68b
kevokagwima <kevokagwima@gmail.com>
2024-06-21 00:37:32 -0700

    Test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - The main function prints out a text
+ * Return: returns a value
 */
-
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"%s\"\n", "Programming is like building a multilingual puzzle");
 return (0);
 }
