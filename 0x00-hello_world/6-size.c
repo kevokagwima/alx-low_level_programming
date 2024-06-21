485857a885168429895ab77423e90dad182008a8
kevokagwima <kevokagwima@gmail.com>
2024-06-21 00:19:10 -0700

    Test

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,6 @@ int main(void)
 {
 printf("Size of int: %zu bytes\n", sizeof(int));
 printf("Size of float: %zu bytes\n", sizeof(float));
-printf("Size of double: %zu bytes\n", sizeof(double));
 printf("Size of long int: %zu bytes\n", sizeof(long int));
 printf("Size of long long int: %zu bytes\n", sizeof(long long int));
 printf("Size of char: %zu bytes\n", sizeof(char));
