2751b7a39423c801bdd3224d3d02a6fcc6c63a25
kevokagwima <kevokagwima@gmail.com>
2024-06-21 00:11:45 -0700

    Test

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - this function uses printf
- */
-
+ * Return: Returns a value
+*/
 int main(void)
 {
-/**
- * printf - prints out a text
-*/
 printf("with proper grammar, but the outcome is a piece of art,\n");
-/**
- * return (0) - return the value 0
-*/
 return (0);
 }
 
