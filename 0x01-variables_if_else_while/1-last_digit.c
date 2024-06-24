2ff15e7f857ab1da996d49072d24a90c3cb87906
kevokagwima <kevokagwima@gmail.com>
2024-06-24 05:40:30 -0700

    Test

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - this function uses printf
+* Return: Returns a value
+*/
+int main()
+{
+srand(time(NULL));
+int n = rand() % (2 * RAND_MAX / 3) - RAND_MAX / 3;
+printf("%d is ", n);
+int last_digit = n % 10;
+if (last_digit > 5)
+{
+printf("and is greater than 5\n");
+}
+else if (last_digit == 0)
+{
+printf("and is 0\n");
+}
+else
+{
+printf("and is less than 6 and not 0\n");
+}
+return 0;
+}
