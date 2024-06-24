7d586239f85b81dcaf94c5567391e87d2cb5d9ae
kevokagwima <kevokagwima@gmail.com>
2024-06-24 06:14:26 -0700

    Test" git push "

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,8 +11,8 @@ int n;
 int last_digit;
 srand(time(NULL));
 n = rand() % 100;
-printf("Last digit of %d is %d is ", n, last_digit);
 last_digit = n % 10;
+printf("Last digit of %d is %d is ", n, last_digit);
 if (last_digit > 5)
 {
 printf("and is greater than 5\n");
