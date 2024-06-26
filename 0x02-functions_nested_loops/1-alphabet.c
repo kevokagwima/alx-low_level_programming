f561ad784a0c7c594319bf5dc24fa78c3140cf46
kevokagwima <kevokagwima@gmail.com>
2024-06-26 01:58:46 -0700

    Test

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
 #include "main.h"
+
 /**
-* main - Main function
-* Return: Return a value
-* print_alphabet - Prints the lowercase alphabet followed by a newline
-*/
-int  main(void)
+ * print_alphabet - Prints the lowercase alphabet followed by a newline
+ */
+void print_alphabet(void)
 {
 char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
@@ -12,4 +11,14 @@ for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
 }
 _putchar('\n');
+}
+
+/**
+ * main - Entry point of the program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+print_alphabet();
+return (0);
 }
